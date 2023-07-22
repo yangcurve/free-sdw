9696f46b832b066e9d7f949ecf271d5d547936f5
yangcurve <72732309+yangcurve@users.noreply.github.com>
2023-07-22 18:55:30 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,13 +9,6 @@ apt -y install -qq unionfs-fuse
 pip install -q torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1+cu118 torchtext==0.15.1 torchdata==0.6.0 --extra-index-url https://download.pytorch.org/whl/cu118 -U
 pip install -q xformers==0.0.19 triton==2.0.0 -U
 
-mkdir /content/fused-models
-mkdir /content/models
-mkdir /content/fused-lora
-mkdir /content/lora
-unionfs-fuse /content/drive/MyDrive/stable-diffusion-webui-colab/stable-diffusion-webui/models/Stable-diffusion=RW:/content/models=RW /content/fused-models
-unionfs-fuse /content/drive/MyDrive/stable-diffusion-webui-colab/stable-diffusion-webui/extensions/sd-webui-additional-networks/models/lora=RW:/content/drive/MyDrive/stable-diffusion-webui-colab/stable-diffusion-webui/models/Lora=RW:/content/lora=RW /content/fused-lora
-
 cd /content/drive/MyDrive/stable-diffusion-webui-colab/stable-diffusion-webui
 
 git reset --hard
